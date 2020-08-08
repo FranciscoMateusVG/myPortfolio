f72ef17105775b13c25e2eabde353d69de86a99b
FranciscoMateusVG <49985463+FranciscoMateusVG@users.noreply.github.com>
2020-08-08 14:58:16 -0300

    forms

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -5,27 +5,31 @@ const contact = () => {
     <section className="contact-page">
       <article className="contact-form">
         <h3>get in touch</h3>
-        <form name="contact portfolio" method="POST" data-netlify="true">
-          <div className="form-group">
-            <input
-              type="text"
-              name="name"
-              placeholder="name"
-              className="form-control"
-            />
-            <input
-              type="email"
-              name="email"
-              placeholder="email"
-              className="form-control"
-            />
-            <textarea
-              name="message"
-              rows="5"
-              placeholder="message"
-              className="form-control"
-            />
-          </div>
+        <form
+          name="contact portfolio"
+          method="POST"
+          data-netlify="true"
+          data-netlify-recaptcha="true"
+        >
+          <input
+            type="text"
+            name="name"
+            placeholder="name"
+            className="form-control"
+          />
+          <input
+            type="email"
+            name="email"
+            placeholder="email"
+            className="form-control"
+          />
+          <textarea
+            name="message"
+            rows="5"
+            placeholder="message"
+            className="form-control"
+          />
+          <div data-netlify-recaptcha="true"></div>
           <button type="submit" className="submit-btn btn">
             Submit Here
           </button>
